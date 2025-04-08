28578694ef4b6ba58ae843a6f0e26a28a7497e13
jtwuuuuu <1874216549@qq.com>
2025-04-08 14:43:32 -0400

    basic and server

--- disk.cpp
+++ disk.cpp
@@ -1,5 +1,23 @@
 #include "disk.h"
 Disk_Server::Disk_Server(int fd):Server(fd){}
+void Disk::_read(){
+
+}
+void Disk::_write(){
+
+}
+void Disk::_delete(){
+
+}
+void Disk::_create(){
+
+}
+
+
+
+/*
+ * @brief debug function
+ * */
 void Disk_Server::print_req(){
   switch (request.rtype) {
     case Rtype::DELETE:std::cout << "DELETE\n";
@@ -16,6 +34,7 @@ void Disk_Server::print_req(){
   for(auto v : request.path)
     std::cout << v << " ";
 }
+
 /*
  * @brief This function handles the requests
  * */
@@ -23,14 +42,40 @@ void Disk_Server::handle(){
   _recv();
   to_req(parse_del(str_in, ' '));
   print_req();
-
   //handle the request
-  /*switch (request.rtype) {*/
-  /*  case Rtype::READ:*/
-  /*  case Rtype::READ:*/
-  /*}*/
+  switch (request.rtype) {
+    case Rtype::READ:{
+      _read();
+      break;
+    }
+    case Rtype::WRITE:{
+      _write();
+      break;
+    }
+    case Rtype::CREATE:{
+      _create();
+      break;
+    }
+    case Rtype::DELETE:{
+      _delete();
+      break;
+    }
+  }
   /**/
   _close();
   //NOTE: This may lead to Segmentation fualt
   delete this;//delete the handler when spawning thread
 }
+
+boost::shared_mutex& Lock::find_lock(std::string str){
+    //first aquiring memory lock
+    boost::lock_guard<boost::mutex> __lock(mem_mt);
+    //aquire shared mutex for read
+    /*boost::shared_lock<boost::shared_mutex> _lock(lock.sd_mt);*/
+    //check if the file exists
+    auto it = file_locks.find(str);
+    if(it != file_locks.end()){
+      return it->second;
+    }
+    throw NofileErr("cannot find file");
+  }
--- disk.h
+++ disk.h
@@ -1,20 +1,35 @@
 #include <asm-generic/socket.h>
+#include <boost/thread/lock_guard.hpp>
+#include <boost/thread/lock_types.hpp>
 #include <boost/thread/pthread/mutex.hpp>
 #include <boost/thread/pthread/shared_mutex.hpp>
 #include "server.h"
 #include "fs_server.h"
+#include <string>
 #include <unistd.h>
 #include <unordered_map>
 #include <vector>
 #include <boost/thread.hpp>
 
+/*struct Read_lock(){*/
+/*  ReadLock(boost::shared_mutex& sd_mt)*/
+/*        : mem_lock(mem_mt), shared_lock(sd_mt) {}*/
+/*}*/
+struct NofileErr{
+  std::string msg;
+  NofileErr(std::string str):msg{str}{}
+};
+
+struct Lock{
+  boost::mutex mem_mt;
+  std::unordered_map<std::string, boost::shared_mutex> file_locks;
+  boost::shared_mutex& find_lock(std::string str);
+};
+
 class Disk{
+  static Lock lock;
+  static std::vector<int>* free_list;
   protected:
-  boost::mutex* mem_mt;
-  std::vector<int>* free_list;
-  std::unordered_map<std::string, boost::shared_mutex>* read_locks;
-  std::unordered_map<std::string, boost::shared_mutex>* write_locks;
-public:
   void _write();
   void _read();
   void _create();
